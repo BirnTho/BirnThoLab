14b85d8dfa3340a0826b3a33567b59f5cbd0cf3a
Birntho <thomas.birngruber@apk.at>
2021-06-29 13:00:31 +0200

    init App

---
+++ myAppBirnTho/myAppBirnTho/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace myAppBirnTho
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
